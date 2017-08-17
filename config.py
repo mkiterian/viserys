4989c8e7c3f7af86040227b872af1538debf746b
mkiterian <hmukite@gmail.com>
2017-08-17 23:49:52 +0300

    [Chore] Set db url and key in env variables

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+import os
+
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
-    SECRET_KEY = 'asdfh87sf454yhggfd45dererfds22as112'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/viserys'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     # SQLALCHEMY_DATABASE_URI = 'postgres://mjpkpslhyxsugc:c96eb886222059ab00d391a4bb957cd624608e6799383e80997847770a8437fe@ec2-50-19-95-47.compute-1.amazonaws.com:5432/dfp3o42ok4g498'
     JWT_AUTH_URL_RULE = '/api/v1/auth/login'
 
